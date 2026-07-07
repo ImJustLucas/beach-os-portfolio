71dd79440f6a0d64056b7e1df3b8396127b1861f
ImJustLucas <lucas.bellier@icloud.com>
2026-07-07 10:53:42 +0200

    chore: normalise les imports de use-translation

--- src/i18n/use-translation.ts
+++ src/i18n/use-translation.ts
@@ -1,7 +1,7 @@
 import { usePreferences } from "@/stores/preferences-store";
 
-import {  translations } from "./translations";
-import type {TranslationKey} from "./translations";
+import { translations } from "./translations";
+import type { TranslationKey } from "./translations";
 
 export function useTranslation() {
   const locale = usePreferences((state) => state.locale);
